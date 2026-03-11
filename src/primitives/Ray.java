997409c30a7236cbdc23cfe90453fc8ca144cac0
levikaprow-afk <levikaprow@gmail.com>
2026-03-11 21:50:54 +0200

    Added Ray class

---
+++ src/primitives/Ray.java
@@ -0,0 +1,34 @@
+package primitives;
+
+/**
+ * Class Ray represents a ray in 3D Cartesian coordinate system.
+ */
+public final class Ray {
+
+    private final Point origin;
+    private final Vector direction;
+
+    /**
+     * Constructor for Ray.
+     *
+     * @param origin starting point
+     * @param direction ray direction
+     */
+    public Ray(Point origin, Vector direction) {
+        this.origin = origin;
+        this.direction = direction.normalize();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {return true;}
+        if (!(obj instanceof Ray)) {return false;}
+        Ray other = (Ray) obj;
+        return this.origin.equals(other.origin) &&
+                this.direction.equals(other.direction);}
+
+    @Override
+    public String toString() {
+        return "Ray: origin=" + origin + ", direction=" + direction;
+    }
+}
